e407614b4f2f0536a0d0e4bf5aa2f9b0756e3710
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-02-10 14:55:12 +0100

    add: paths for handlers as constants

--- handlers/constants.go
+++ handlers/constants.go
@@ -1,5 +1,11 @@
 package handlers
 
-//URL const
+//Version
+const VERSION = "v1"
 
+
+//URL const
 const DEFAULT_PATH = "/"
+const UNI_INFO_PATH = "/unisearcher/" + VERSION +"/uniinfo/"
+const NEIGHBOUR_UNIS_PATH = "/unisearcher/"+ VERSION +"/neighbourunis/"
+const DIAG_PATH = "/unisearcher/" + VERSION +"/diag/"
